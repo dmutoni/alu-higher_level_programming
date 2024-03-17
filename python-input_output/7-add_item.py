339c56cabd29f25320f36df14d8cfeb741dde24f
Denyse Mutoni Uwingeneye <mdenyse15@gmail.com>
2024-03-17 09:52:20 -0700

    Revert file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+"""  Adds all arguments to a Python list, and then save them to a file
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+file = "add_item.json"
+
+try:
+    arg_list = load_from_json_file(file)
+except FileNotFoundError:
+    arg_list = list()
+
+for arg in argv[1:]:
+    arg_list.append(arg)
+
+save_to_json_file(arg_list, file)
+
