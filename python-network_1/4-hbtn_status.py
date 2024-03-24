10b9f0e65d06c42f5e0f16d13cf1e90f2b2c515a
dmutoni <mdenyse15@gmail.com>
2024-03-24 15:02:37 +0000

    Add question 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
-import requests
+import urllib.request
+
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-
-    res = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
